41466833f9dbcfb06735c9ce303215ebcee1c35c
Azamat <azamatovuser@gmail.com>
2024-12-25 19:33:53 +0500

    settings changed

--- config/urls.py
+++ config/urls.py
@@ -24,7 +24,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Expense",
+        title="Easy-cv",
         default_version='v1',
         description="Django project",
         terms_of_service="https://www.google.com/policies/terms/",
